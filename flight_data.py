bdf180065f5e39b555183429927c73d2be79e0a2
Milan Solanki <66133186+freshprinceofcoders@users.noreply.github.com>
2021-08-15 23:31:25 +0100

    reads from the file and gets the destanation and calls the api

--- flight_data.py
+++ flight_data.py
@@ -1,14 +1,26 @@
 import requests
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/GBP/en-US/LHR/DEL/2021-08-15"
+def readFile(fileName):
+    fileObj = open(fileName, "r") #opens the file in read mode
+    user_input = fileObj.read().splitlines() #puts the file into an array
+    fileObj.close()
 
-querystring = {"inboundpartialdate":"2021-08-28"}
+    url = f"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-US/{user_input[3]}/{user_input[2]}/{user_input[4]}"
+    print (url)
+    querystring = {"inboundpartialdate":user_input[5]}
 
-headers = {
-    'x-rapidapi-key': "c5793d7cafmshf6f023ddc0f0f5ep11116cjsndb8f319ec001",
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-    }
+    headers = {
+        'x-rapidapi-key': "c5793d7cafmshf6f023ddc0f0f5ep11116cjsndb8f319ec001",
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
 
-print(response.text)
+readFile('flightData.txt')
+
+
+
+
+
+
