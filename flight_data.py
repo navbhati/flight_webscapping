deec3671fbeadf79887da890744e043d199cca4d
Milan Solanki <66133186+freshprinceofcoders@users.noreply.github.com>
2021-08-17 22:50:08 +0100

    this gets the price the api returns and prints out the price if it is less than the input

--- flight_data.py
+++ flight_data.py
@@ -1,4 +1,5 @@
 import requests
+import  json
 
 def readFile(fileName):
     fileObj = open(fileName, "r") #opens the file in read mode
@@ -6,7 +7,7 @@ def readFile(fileName):
     fileObj.close()
 
     url = f"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/UK/GBP/en-US/{user_input[3]}/{user_input[2]}/{user_input[4]}"
-    print (url)
+    #print (url)
     querystring = {"inboundpartialdate":user_input[5]}
 
     headers = {
@@ -15,9 +16,19 @@ def readFile(fileName):
         }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
+    the_info = response.json()
+    #print(the_info['Quotes'])
+    #print(the_info['Quotes'][0]["MinPrice"])
+    
+    i=0
+    #print(int(user_input[8]))
+    for i in range(len(the_info['Quotes'])):
+        if int(user_input[8]) >= int(the_info['Quotes'][i]["MinPrice"]):
+            print(int(the_info['Quotes'][i]['MinPrice']))            
+        i+=1
+readFile('flightData.json')
 
-readFile('flightData.txt')
+
 
 
 
